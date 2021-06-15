44f5508d1ec1fd0df17255443bb835e08b581897
Suhaib Khater <suhaibkhater@pop-os.localdomain>
2021-06-15 17:53:48 +0300

    fix

--- src/App.js
+++ src/App.js
@@ -1,7 +1,6 @@
 import './App.css';
 import React from 'react';
 import { BrowserRouter } from 'react-router-dom';
-import 'bootstrap/dist/css/bootstrap.css';
 import Root from "./components/Root";
 
 const App = () => {
